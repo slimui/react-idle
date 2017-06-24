20a0c43f18250dca2f0d1b3a6b599653df44e323
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2017-06-24 13:00:01 -0400

    Clean up code a bit in App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function increment(state, props) {
-  return {
-    value: state.value + 1/60
-  };
+    return {
+        value: state.value + 1/60
+    };
 }
 
 class App extends Component {
 
-  state = { value: 0};
+    state = { value: 0};
 
-  componentWillMount() {
-    
-      setInterval(this.timer, 1000/60);
-    
-  }
+    componentWillMount() {
+        setInterval(this.timer, 1000/60);
+    }
 
-  timer = ()  => {
-    this.setState(increment);
-  }
+    timer = ()  => {
+        this.setState(increment);
+    }
 
-  render() {
-
-    let count = this.state.value.toFixed(1);
-
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          {count}
-        </p>
-      </div>
-    );
-  }
+    render() {
+        let count = this.state.value.toFixed(1);
+        return (
+            <div className="App">
+                {count}
+            </div>
+        );
+    }
 }
 
 export default App;
