b5195d7848d67ab7589e37b83ad83aa773b892d2
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2017-06-24 04:17:11 -0400

    Example of a basic counter ticking up each second

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function increment(state, props) {
+  return {
+    value: state.value + 1
+  };
+}
+
 class App extends Component {
+
+  state = { value: 0};
+
+  componentWillMount() {
+    if(this.state.value === 0) {
+      setInterval(this.timer, 1000);
+    }
+  }
+
+  timer = ()  => {
+    this.setState(increment);
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +30,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.value}
         </p>
       </div>
     );
