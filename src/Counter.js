fdcda71db6f4759ef2a9f258f044a0efed2762ef
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2017-07-01 15:03:00 -0400

    Move offline progress calculation code to separate file for later use

--- src/Counter.js
+++ src/Counter.js
@@ -10,45 +10,11 @@ class Counter extends Component {
     render() {
         const {count, valuePerSecond} = this.props;
         let formattedCount = count.toFixed(5);
-
-        let stores = {
-            count: 1,
-            value: 1
-        }
-        let factories = {
-            count: 1,
-            value: 1
-        };
-        let balls = 0;
-
-        let calculateProgress = function(time) {
-
-            let tickDuration = 1000/60;
-            let ticks = time/tickDuration;
-            console.log("ticks:", ticks, "tick duration:", tickDuration);
-
-            let calculation = 0;
-            let storeCount = stores.count;
-            let factoriesCount = factories.count;
-            let storeProduction = stores.value;
-            let factoryProduction = factories.value;
-
-            for(let i = 0; i < ticks; i++) {
-                storeCount += (factoriesCount * factoryProduction) * (tickDuration/1000);
-                calculation += (storeCount * storeProduction) * (tickDuration/1000);
-            }
-
-            return calculation;
-        };
-
-        balls = calculateProgress(2000);
-
-
+        
         return (
             <div>
                 <h1>Counter: {formattedCount}</h1>
                 <h2>Increase Per Second: {valuePerSecond}</h2>
-                <h3>Balls: {balls}</h3>
             </div>
         );
     }
---
+++ src/helpers/index.js
@@ -0,0 +1,35 @@
+export function calculateProgress() {
+    let stores = {
+        count: 1,
+        value: 1
+    }
+    let factories = {
+        count: 1,
+        value: 1
+    };
+    let balls = 0;
+
+    let calculate = function(time) {
+
+        let tickDuration = 1000/60;
+        let ticks = time/tickDuration;
+        console.log("ticks:", ticks, "tick duration:", tickDuration);
+
+        let calculation = 0;
+        let storeCount = stores.count;
+        let factoriesCount = factories.count;
+        let storeProduction = stores.value;
+        let factoryProduction = factories.value;
+
+        for(let i = 0; i < ticks; i++) {
+            storeCount += (factoriesCount * factoryProduction) * (tickDuration/1000);
+            calculation += (storeCount * storeProduction) * (tickDuration/1000);
+        }
+
+        return calculation.toFixed(1);
+    };
+
+    balls = calculate(2000);
+}
+
+        
