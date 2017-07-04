5cfb92573cee39dd365569979c67d43cead99134
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2017-07-04 18:47:29 -0400

    Style the game container using styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { Layout, Menu, Col, Row } from 'antd';
+import Styled from 'styled-components';
 import './App.css';
 
 import Counter from './Counter';
 
 const SubMenu = Menu.SubMenu;
 const { Header, Content, Footer } = Layout;
+
+const FooterContainer = Styled.div`
+    text-align: center;
+`;
+
+const GameContainer = Styled.div`
+    padding: 50px;
+    text-align: center;
+    background: #fff;
+    margin-top: 20px;
+    margin-left: 20px;
+    margin-right: 20px;
+`;
 
 export function increment(deltaTime) {
     return function update(state) {
@@ -28,19 +42,22 @@ class App extends Component {
                     >
                         <Menu.Item key="1">Home</Menu.Item>
                         <Menu.Item key="2">About</Menu.Item>
+                        <Menu.Item key="3">Options</Menu.Item>
                     </Menu>
                 </Header>
                 <Content>
-                    <div style={{background: '#fff', textAlign: 'center'}}>
+                    <GameContainer>
                         <Row>
                             <Col span={12} offset={6}>
                                 <Counter />
                             </Col>
                         </Row>
-                    </div>
+                    </GameContainer>
                 </Content>
                 <Footer>
-                    A game by Sonicrida
+                    <FooterContainer>
+                        A game by Sonicrida
+                    </FooterContainer>
                 </Footer>
             </Layout>
         );
